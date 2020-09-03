0697f5e96cd43989d64d060dcbeec7d3cc2d1e7f
maqq <maqq@retechcorp.com>
2020-09-03 17:01:30 +0800

    first commit

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: 'Jinxi Group',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false
+}
